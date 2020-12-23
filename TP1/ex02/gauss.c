0da28c32385aa64fbd3ecd02ef92b95101103258
AnassBoutslighoi <dell@DESKTOP-82UNC7P.localdomain>
2020-12-23 16:28:41 +0100

    added Exercice02 gauss.c

--- TP1/ex02/gauss.c
+++ TP1/ex02/gauss.c
@@ -3,21 +3,50 @@
 #define N 10
 
 float *remontee(float A[N][N], float B[N], int n)
-{
+{		
+	/******Implement the solution here******/
+	/***************************************/
+	float *X;
+	int i,j;
+	float s;
+	X = malloc(sizeof(float)*n);	
 
-  /******Implement the solution here******/
-  /***************************************/
-  
-  /***************************************/
-  return x;
+	X[n-1] = B[n-1]/A[n-1][n-1];
+	for(i= n-2 ; i>-1 ; i--)
+	{
+		s = 0 ;
+		for (j=i+1 ; j<n;j++)
+		{
+			s = s + A[i][j]*X[j];
+		}
+		X[i] = (B[i] - s)/A[i][i];
+	}
+
+	/***************************************/
+	return X;
 }
 
 float *gauss(float A[N][N] ,float B[N], int n)
 {
-  /******Implement the Gaussian elimination******/
-  /**********************************************/
-  
-
+	/******Implement the Gaussian elimination******/
+	/**********************************************/
+	int i,j,k;
+	float s;
+	float *x;
+	for (k = 0 ; i<n+1 ; k++)
+	{
+		for(i = k+1 ; i<n ; i++)
+		{
+			s  = A[i][k] /A[k][k];
+			//printf(" \n %f",s); 
+			for (j = 0 ; j <n ; j++)
+			{ 
+				A[i][j] = A[i][j] - s * A[k][j] ;
+			//	printf(" \n %f A[%d][%d] ",A[i][j],i,j); 
+			}
+			B[i] = B[i] - s * B[k];
+		}
+	}
   /**********************************************/
   /*Resolve the system using the back substitution method*/
   /*You can use your solution of the exercice 1-b*/
@@ -61,4 +90,3 @@ int main()
   for (int i = 0; i < n; i++)
     printf("%f%c", x[i], ",]"[i == n - 1]);
 }
-
